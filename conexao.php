f2e26328a1fd1c602769664dcc41deb5b75f21b0
Luiz Faitn <faitndelima@gmail.com>
2022-05-03 20:13:18 -0300

    Criação formulário cliente

---
+++ conexao.php
@@ -0,0 +1,11 @@
+<?php
+
+$hostname = "localhost";
+$usuario  = "root";
+$senha    = "9548";
+$banco    = "mydb";
+
+$conexao = new mysqli($hostname, $usuario, $senha, $banco);
+if ($conexao->connect_errno) {
+    echo "Falha ao conectar: (" . $conexao->connect_errno . ") " . $conexao->connect_error;
+}

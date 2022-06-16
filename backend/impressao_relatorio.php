338ceed7404d511011b02e110855e7f2bfc9a568
Luiz Faitn <faitndelima@gmail.com>
2022-06-16 20:45:56 -0300

    Atualizado nome na impressão do relatório.

--- backend/impressao_relatorio.php
+++ backend/impressao_relatorio.php
@@ -38,7 +38,14 @@ if (isset($_GET)) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PDF</title>
+  <title>
+    <?php if ($_GET) {
+      echo $relatorio['modelo'];
+      echo "_";
+      echo $relatorio['qtdItensPedido'];
+    }
+    ?>
+  </title>
   <style>
   * {
     margin: 0;

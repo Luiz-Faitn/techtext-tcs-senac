07e656e4b5354ca40a4509f0e66997e959aa1423
Suelen Francisco Marques <59808425+Suelen-Francisco-Marqes@users.noreply.github.com>
2022-07-03 14:24:42 -0300

    chat

--- backend/chat.php
+++ backend/chat.php
@@ -1,1 +1,14 @@
 <?php
+include "conexao.php";
+		$nome = $_POST['nome'];
+		$mensagem = $_POST['mensagem'];
+
+        $sql = "INSERT INTO chat (nome, mensagem) VALUES ('$nome', '$mensagem')";
+
+        $resultado = mysqli_query($sql, $conexao);
+
+      if ($resultado) {   
+          header('../index.php');
+      } else {
+          echo "Erro: " . mysqli_error($conexao);
+      }

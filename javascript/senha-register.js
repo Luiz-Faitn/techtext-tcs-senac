75630a43f841dd894fc819937d85a79a69e66ed3
Luiz Faitn <faitndelima@gmail.com>
2022-07-06 12:40:26 -0300

    Adicionado Javascript mostrar senha no registro.

---
+++ javascript/senha-register.js
@@ -0,0 +1,12 @@
+const pswrdField = document.querySelector(".former input[type='password']"),
+  toggleIcon = document.querySelector('.former .input-cadastro i');
+
+toggleIcon.onclick = () => {
+  if (pswrdField.type == 'password') {
+    pswrdField.type = 'text';
+    toggleIcon.classList.add('active');
+  } else {
+    pswrdField.type = 'password';
+    toggleIcon.classList.remove('active');
+  }
+};

293805923718298c5b4dbd22471a69c794ee8b26
Luiz Faitn <faitndelima@gmail.com>
2022-07-07 08:46:57 -0300

    Javascript de mostrar a senha no registo de usuários.

--- javascript/senha-register.js
+++ javascript/senha-register.js
@@ -2,7 +2,7 @@ const pswrdField = document.querySelector(".former input[type='password']"),
   toggleIcon = document.querySelector('.former .input-cadastro i');
 
 toggleIcon.onclick = () => {
-  if (pswrdField.type == 'password') {
+  if (pswrdField.type === 'password') {
     pswrdField.type = 'text';
     toggleIcon.classList.add('active');
   } else {

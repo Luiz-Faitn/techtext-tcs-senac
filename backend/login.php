c327aafb51813502acbdf9294bc3ec147d1c0923
Luiz Faitn <faitndelima@gmail.com>
2022-07-07 08:44:45 -0300

    Função de logar no sistema.

--- backend/login.php
+++ backend/login.php
@@ -1,29 +1,31 @@
 <?php
 session_start();
 include_once "conexao.php";
-$nome = mysqli_real_escape_string($conn, $_POST['nome']);
-$senha = mysqli_real_escape_string($conn, $_POST['senha']);
+$nome = mysqli_real_escape_string($conexao, $_POST['nome']);
+$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+
 if (!empty($nome) && !empty($senha)) {
-    $sql = mysqli_query($conn, "SELECT * FROM users WHERE nome = '{$nome}'");
+    $sql = mysqli_query($conexao, "SELECT * FROM usuarios WHERE nome = '{$nome}'");
     if (mysqli_num_rows($sql) > 0) {
         $row = mysqli_fetch_assoc($sql);
         $user_pass = md5($senha);
         $enc_pass = $row['senha'];
+
         if ($user_pass === $enc_pass) {
-            $status = "Active now";
-            $sql2 = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id = {$row['unique_id']}");
+            $status = "Ativo";
+            $sql2 = mysqli_query($conexao, "UPDATE usuarios SET status = '{$status}' WHERE id_unico = {$row['id_unico']}");
             if ($sql2) {
-                $_SESSION['unique_id'] = $row['unique_id'];
-                echo "success";
+                $_SESSION['id_unico'] = $row['id_unico'];
+                echo "Sucesso!";
             } else {
-                echo "Something went wrong. Please try again!";
+                echo "Alguma coisa deu errado. Por favor, tento novamente!";
             }
         } else {
-            echo "nome or senha is Incorrect!";
+            echo "Nome ou senha estão incorretos.";
         }
     } else {
-        echo "$nome - This nome not Exist!";
+        echo "$nome - Esse nome de usuário não existe!";
     }
 } else {
-    echo "All input fields are required!";
+    echo "Todos os campos devem estar preenchidos!";
 }
